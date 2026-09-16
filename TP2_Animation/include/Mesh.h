b112f784def557bebbc8caeb3f7095f75131a98a
yaqinoel <yaqizhang.unique@outlook.com>
2026-09-16 15:51:50 +0200

    import TP2 projet

---
+++ TP2_Animation/include/Mesh.h
@@ -0,0 +1,81 @@
+#ifndef MESH_H
+#define MESH_H
+
+
+#include <vector>
+#include <string>
+#include "Vec3.h"
+#include "Skeleton.h"
+
+#include <cmath>
+
+#include "GLHeaders.h"
+
+
+// -------------------------------------------
+// Basic Mesh class
+// -------------------------------------------
+
+struct MeshVertex {
+    inline MeshVertex () {
+        w.clear();
+    }
+    inline MeshVertex (const Vec3 & _p, const Vec3 & _n) : p (_p), n (_n) {
+        w.clear();
+    }
+    inline MeshVertex (const MeshVertex & vertex) : p (vertex.p), n (vertex.n) , w(vertex.w) {
+    }
+    inline virtual ~MeshVertex () {}
+    inline MeshVertex & operator = (const MeshVertex & vertex) {
+        p = vertex.p;
+        n = vertex.n;
+        w = vertex.w;
+        return (*this);
+    }
+    // membres :
+    Vec3 p; // une position
+    Vec3 n; // une normale
+    std::vector< double > w; // skinning weights
+};
+
+struct MeshTriangle {
+    inline MeshTriangle () {
+        v[0] = v[1] = v[2] = 0;
+    }
+    inline MeshTriangle (const MeshTriangle & t) {
+        v[0] = t.v[0];   v[1] = t.v[1];   v[2] = t.v[2];
+    }
+    inline MeshTriangle (unsigned int v0, unsigned int v1, unsigned int v2) {
+        v[0] = v0;   v[1] = v1;   v[2] = v2;
+    }
+    inline virtual ~MeshTriangle () {}
+    inline MeshTriangle & operator = (const MeshTriangle & t) {
+        v[0] = t.v[0];   v[1] = t.v[1];   v[2] = t.v[2];
+        return (*this);
+    }
+    // membres :
+    unsigned int v[3];
+};
+
+
+
+
+class Mesh {
+public:
+    std::vector<MeshVertex> V;
+    std::vector<MeshTriangle> T;
+
+    void loadOFF (const std::string & filename);
+    void recomputeNormals ();
+
+    void compute_skinning_weights( Skeleton & skeleton );
+
+    void draw( int displayedBone ) const ;
+    void drawTransformedMesh( SkeletonTransformation & transfo ) const ;
+
+    Vec3 HSVtoRGB( float fH, float fS, float fV) const;
+};
+
+
+
+#endif
---
+++ TP2_Animation/src/Mesh.cpp
@@ -0,0 +1,174 @@
+#include "Mesh.h"
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+void Mesh::loadOFF (const std::string & filename) {
+    std::ifstream in (filename.c_str ());
+    if (!in)
+        exit (EXIT_FAILURE);
+    std::string offString;
+    unsigned int sizeV, sizeT, tmp;
+    in >> offString >> sizeV >> sizeT >> tmp;
+    V.resize (sizeV);
+    T.resize (sizeT);
+    for (unsigned int i = 0; i < sizeV; i++)
+        in >> V[i].p;
+    int s;
+    for (unsigned int i = 0; i < sizeT; i++) {
+        in >> s;
+        for (unsigned int j = 0; j < 3; j++)
+            in >> T[i].v[j];
+    }
+    in.close ();
+    recomputeNormals ();
+}
+
+void Mesh::recomputeNormals () {
+    for (unsigned int i = 0; i < V.size (); i++)
+        V[i].n = Vec3 (0.0, 0.0, 0.0);
+    for (unsigned int i = 0; i < T.size (); i++) {
+        Vec3 e01 = V[T[i].v[1]].p -  V[T[i].v[0]].p;
+        Vec3 e02 = V[T[i].v[2]].p -  V[T[i].v[0]].p;
+        Vec3 n = Vec3::cross (e01, e02);
+        n.normalize ();
+        for (unsigned int j = 0; j < 3; j++)
+            V[T[i].v[j]].n += n;
+    }
+    for (unsigned int i = 0; i < V.size (); i++)
+        V[i].n.normalize ();
+}
+
+void Mesh::compute_skinning_weights( Skeleton & skeleton ) {
+    //---------------------------------------------------//
+    //---------------------------------------------------//
+    // code to change :
+
+    // Indications:
+    // you should compute weights for each vertex w.r.t. the skeleton bones
+    // so each vertex will have B weights (B = number of bones)
+    // these weights shoud be stored in vertex.w:
+
+
+    //---------------------------------------------------//
+    //---------------------------------------------------//
+    //---------------------------------------------------//
+}
+
+void Mesh::draw( int displayedBone ) const {
+
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+    glBegin (GL_TRIANGLES);
+    for (unsigned int i = 0; i < T.size (); i++)
+        for (unsigned int j = 0; j < 3; j++) {
+            const MeshVertex & v = V[T[i].v[j]];
+            if( displayedBone >= 0 && displayedBone < v.w.size() ) {
+                Vec3 rgb = HSVtoRGB( v.w[displayedBone], 0.8,0.8 );
+                glColor3f( rgb[0], rgb[1], rgb[2] );
+            } else glColor3f( 0.6, 0.6, 0.6 );
+            glNormal3f (v.n[0], v.n[1], v.n[2]);
+            glVertex3f (v.p[0], v.p[1], v.p[2]);
+        }
+
+    glEnd ();
+}
+
+void Mesh::drawTransformedMesh( SkeletonTransformation & transfo ) const {
+
+
+    std::vector< Vec3 > new_positions( V.size() );
+    std::vector< Vec3 > new_normals( V.size() );
+
+    //---------------------------------------------------//
+    //---------------------------------------------------//
+    // code to change :
+    for( unsigned int i = 0 ; i < V.size() ; ++i ) {
+        Vec3 p = V[i].p;
+        Vec3 n = V[i].n;
+
+        // Indications:
+        // you should use the skinning weights to blend the transformations of the vertex position by the bones.
+
+        new_positions[ i ] = p;
+        new_normals[ i ] = n;
+    }
+    //---------------------------------------------------//
+    //---------------------------------------------------//
+    //---------------------------------------------------//
+
+    glEnable(GL_LIGHTING);
+    glBegin (GL_TRIANGLES);
+    for (unsigned int i = 0; i < T.size (); i++)
+        for (unsigned int j = 0; j < 3; j++) {
+            const MeshVertex & v = V[T[i].v[j]];
+            Vec3 p = new_positions[ T[i].v[j] ];
+            Vec3 n = new_normals[ T[i].v[j] ];
+            glColor3f( 0.6, 0.6, 0.6 );
+            glNormal3f (n[0], n[1], n[2]);
+            glVertex3f (p[0], p[1], p[2]);
+        }
+    glEnd ();
+}
+
+/*! \brief Convert HSV to RGB color space
+
+  Converts a given set of HSV values `h', `s', `v' into RGB
+  coordinates. The output RGB values are in the range [0, 1], and
+  the input HSV values are in the ranges h = [0, 360], and s, v =
+  [0, 1], respectively.
+
+  \param fH Hue component, used as input, range: [0, 1]
+  \param fS Hue component, used as input, range: [0, 1]
+  \param fV Hue component, used as input, range: [0, 1]
+
+  \param fR Red component, used as output, range: [0, 1]
+  \param fG Green component, used as output, range: [0, 1]
+  \param fB Blue component, used as output, range: [0, 1]
+
+*/
+Vec3 Mesh::HSVtoRGB( float fH, float fS, float fV) const {
+
+    fH=(1.-fH)*0.65*360.;
+
+    float fR, fG, fB;
+    float fC = fV * fS; // Chroma
+    float fHPrime = fmod(fH / 60.0, 6);
+    float fX = fC * (1 - fabs(fmod(fHPrime, 2) - 1));
+    float fM = fV - fC;
+
+    if(0 <= fHPrime && fHPrime < 1) {
+        fR = fC;
+        fG = fX;
+        fB = 0;
+    } else if(1 <= fHPrime && fHPrime < 2) {
+        fR = fX;
+        fG = fC;
+        fB = 0;
+    } else if(2 <= fHPrime && fHPrime < 3) {
+        fR = 0;
+        fG = fC;
+        fB = fX;
+    } else if(3 <= fHPrime && fHPrime < 4) {
+        fR = 0;
+        fG = fX;
+        fB = fC;
+    } else if(4 <= fHPrime && fHPrime < 5) {
+        fR = fX;
+        fG = 0;
+        fB = fC;
+    } else if(5 <= fHPrime && fHPrime < 6) {
+        fR = fC;
+        fG = 0;
+        fB = fX;
+    } else {
+        fR = 0;
+        fG = 0;
+        fB = 0;
+    }
+
+    fR += fM;
+    fG += fM;
+    fB += fM;
+    return Vec3(fR,fG,fB);
+}
